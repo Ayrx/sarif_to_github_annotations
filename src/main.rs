deddcfa72dbeadcee5962f326e43237cd6eb1440
Terry Chia <terry@ayrx.me>
2022-07-30 19:49:37 +0800

    Implement argument parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    file: PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
