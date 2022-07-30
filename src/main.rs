a05de3c16ca1e6e22da71fed5061fb38640a92ed
Terry Chia <terry@ayrx.me>
2022-07-30 19:53:37 +0800

    Implement Sarif parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use anyhow::Result;
 use clap::Parser;
+use serde_sarif::sarif::Sarif;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    file: PathBuf
+    file: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
+
+    let file = File::open(args.file)?;
+    let reader = BufReader::new(file);
+
+    let sarif: Sarif = serde_json::from_reader(reader)?;
+
+    Ok(())
 }
